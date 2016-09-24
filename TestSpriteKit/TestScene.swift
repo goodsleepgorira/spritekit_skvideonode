f953efa12f4bbed47d4c11d3528d1300eb8db147
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-09-24 00:42:49 -0700

    Test

--- TestSpriteKit/TestScene.swift
+++ TestSpriteKit/TestScene.swift
@@ -6,47 +6,23 @@ import Foundation
 import SpriteKit
 
 class TestScene:SKScene {
-
-    var monkey:SKSpriteNode!
-    var speaker:SKSpriteNode!
-    
-    var targetNode:SKSpriteNode!
     
     //現在シーン設定時の呼び出しメソッド
     override func didMoveToView(view: SKView) {
-        
-        //SKSファイルに配置したノードを取得する。
-        monkey = self.childNodeWithName("monkey") as? SKSpriteNode
-        speaker = self.childNodeWithName("speaker") as? SKSpriteNode
-    }
 
+       //シーンに配置するノードを作成する。
+       let background = SKSpriteNode(imageNamed: "field")
+       let monkey = SKSpriteNode(imageNamed: "monkey")
+       let bird = SKSpriteNode(imageNamed: "bird_brown")
+       
+       //ノードの座標を指定する。
+       background.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+       monkey.position =  CGPoint(x: self.frame.width/3, y: self.frame.height/5)
+       bird.position = CGPoint(x: self.frame.width*2/3, y: self.frame.height*3/5)
 
-
-    //画面タッチ開始時の呼び出しメソッド
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-
-        //タッチしたノードを取得する。
-        let location = touches.first!.locationInNode(self)
-        let node = nodeAtPoint(location) as! SKSpriteNode
-        
-        if(node == monkey || node == speaker) {
-            //タッチしたノードをターゲットに設定する。
-            targetNode = node
-        }
-    }
-
-
-    
-    //画面タッチ移動時の呼び出しメソッド
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-
-        //移動後の座標を取得する。
-        let location = touches.first!.locationInNode(self)
-        
-        //移動後の座標に移動するアクションを作成する。
-        let action = SKAction.moveTo(CGPoint(x:location.x, y:location.y), duration:0.1)
-            
-        //アクションを実行する。
-        targetNode.runAction(action)
+       //ノードをシーンに配置する。
+       self.addChild(background)
+       self.addChild(monkey)
+       self.addChild(bird)
     }
 }
