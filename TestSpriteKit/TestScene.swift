69ffe1d9ec0e32570d4e3c518ecd1b25ef0d760f
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-09-26 14:31:33 +1200

    Test

--- TestSpriteKit/TestScene.swift
+++ TestSpriteKit/TestScene.swift
@@ -12,17 +12,20 @@ class TestScene:SKScene {
 
        //シーンに配置するノードを作成する。
        let background = SKSpriteNode(imageNamed: "field")
-       let monkey = SKSpriteNode(imageNamed: "monkey")
+       let monkey1 = SKSpriteNode(imageNamed: "monkey")
+       let monkey2 = SKSpriteNode(imageNamed: "monkey")
        let bird = SKSpriteNode(imageNamed: "bird_brown")
        
        //ノードの座標を指定する。
        background.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
-       monkey.position =  CGPoint(x: self.frame.width/3, y: self.frame.height/5)
-       bird.position = CGPoint(x: self.frame.width*2/3, y: self.frame.height*3/5)
+       monkey1.position =  CGPoint(x: self.frame.width*1/3, y: self.frame.height/2)
+       monkey2.position =  CGPoint(x: self.frame.width*2/3, y: self.frame.height/2)
+       bird.position = CGPoint(x: self.frame.width*2/3, y: self.frame.height*4/5)
 
        //ノードをシーンに配置する。
        self.addChild(background)
-       self.addChild(monkey)
+       self.addChild(monkey1)
+       self.addChild(monkey2)
        self.addChild(bird)
     }
 }
